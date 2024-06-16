1b342de4a4423c2934a4316a47c3c1f81f84b3b8
unknown <rajgeerpaswan123@gmail.com>
2024-06-16 19:25:40 +0530

    update pacckage.json

--- BackendTask/index.js
+++ BackendTask/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('./routes');
+const router = require('./src/routes');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
--- BackendTask/src/routes/index.js
+++ BackendTask/src/routes/index.js
@@ -3,4 +3,7 @@ const loadBalancerMiddleware = require('../middlewares');
 const router = express.Router();
 
 router.use(loadBalancerMiddleware);
+router.get('/home', (req, res) => {
+    res.send("Welcome in home");
+})
 module.exports = router;
